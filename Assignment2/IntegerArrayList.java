c10174e0d8e90e3c9f579af8f7a3f319ae7304f0
smoreno11 <60587113+smoreno11@users.noreply.github.com>
2023-12-19 21:42:55 -0800

    Created functions
    createIntegerArrayList functions

--- Assignment2/IntegerArrayList.java
+++ Assignment2/IntegerArrayList.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class IntegerArrayList {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    int seed = 0;
+
+    Scanner input = new Scanner(System.in);
+    ArrayList<Integer> myIntAL = createIngerArrayList();
+    System.out.println("Please enter an integer seed between 1 and 10 inclusively");
+    seed = input.nextInt();
+    while(seed <= 1 || seed >= 10)
+    { 
+      System.out.println("Error! Please enter an integer seed between 1 and 10 inclusively");   
+      seed = input.nextInt();
+    }
+    
+    Random randy = new Random();
+    randy.setSeed(seed); 
+    
   }
+
+  public static ArrayList<Integer> createIngerArrayList(){
+    ArrayList<Integer> genericIntAL = new ArrayList<Integer>();
+    return genericIntAL;
+  }
+
 }
